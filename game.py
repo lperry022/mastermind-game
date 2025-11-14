dd461e4d43ee0aaec14cead11cd2673efdbd2ebb
Liana Perry <62174756+lperry022@users.noreply.github.com>
2025-11-14 17:44:07 +1000

    Part 3 + 4: Checking guesses and running the game

--- game.py
+++ game.py
@@ -55,5 +55,60 @@ def guess_code():
     return guess
     # we return the guess list so that we can use it later in the game
 
-
 # -----------------------END OF PART 2: ALLOW THE USER TO GUESS THE CODE-----------------------#
+
+# -----------------------START OF PART 3: CHECK INCORRECT AND CORRECT GUESSES-----------------------#
+
+
+def check_code(guess, real_code):
+    # the purpose of this function is to take the colour in the user's guess and compare it to the real code
+    colour_counts = {}
+    correct_position = 0
+    incorrect_position = 0
+# we create an empty dictionary to store the counts of each colour in the real code
+# we also create two variables to store the number of colours that are in the correct position and the number of colours that are in the incorrect position
+    for colour in real_code:
+        if colour not in colour_counts:
+            colour_counts[colour] = 0
+            colour_counts[colour] += 1
+# this for loop counts the number of times each colour appears in the real code and stores it in a dictionary
+
+    for guess, real_colour in zip(guess, real_code):
+        # zip() function is used to combine two lists into a list of tuples
+        # for example, if guess = ["Red", "Blue", "Green", "Yellow"] and real_code = ["Blue", "Red", "Green", "Yellow"]
+        if guess == real_colour:
+            correct_position += 1
+            colour_counts[guess] -= 1
+# this for loop compares the user's guess to the real code and counts the number of colours that are in the correct position
+# this removes the colour from the dictionary so that it is not counted again in the next for loop
+        for guess_colour, real_colour in zip(guess, real_code):
+            if guess_colour in colour_counts and colour_counts[guess_colour] > 0:
+                incorrect_position += 1
+                colour_counts[guess_colour] -= 1
+# this for loop compares the user's guess to the real code and counts the number of colours that are in the incorrect position
+# this checks if the colour is in the dictionary and if the count is greater than 0
+    return correct_position, incorrect_position
+    # we return the number of colours that are in the correct position and the number of colours that are in the incorrect position
+
+# -----------------------END OF PART 3: CHECK INCORRECT AND CORRECT GUESSES-----------------------#
+
+# -----------------------START OF PART 4: RUN THE GAME-----------------------#
+
+
+def game():
+    code = generate_code()
+    for attempt in range(1, TRIES + 1):
+        print(f"Attempt {attempt} of {TRIES}")
+# we print the attempt number and the total number of tries
+        guess = guess_code()
+# we call the guess_code function to get the user's guess
+        correct_position, incorrect_position = check_code(guess, code)
+# we call the check_code function to compare the user's guess to the real code
+        print(
+            f"Correct Position: {correct_position}, Incorrect Position: {incorrect_position}")
+        if correct_position == CODE_LENGTH:
+            print("Congratulations! You've guessed the code!")
+            break
+# if the user has guessed all 4 colours in the correct position, they win the game
+# if the user has not guessed the code in the given number of tries, they lose the game
+# -----------------------END OF PART 4: RUN THE GAME-----------------------#
